545ece2934a0aab2449b92d71581ec5f8740e5c0
James Trimble <james.trimble@yahoo.co.uk>
2021-11-19 14:17:47 +0000

    Fix some filenames

--- 14-mcsplit-i-undirected/modified-mcsplit-experiment/make-random-instances3.sh
+++ 14-mcsplit-i-undirected/modified-mcsplit-experiment/make-random-instances3.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-mkdir -p random-instances2
+mkdir -p random-instances3
 
-rm -f randomplaininstances2.txt
+rm -f randomplaininstances3.txt
 
 for i in $(seq 1 1000); do
     echo $i
-    python random_graph.py 10-40 0.3 > random-instances2/r${i}A.grf
-    python random_graph.py 10-40 0.3 > random-instances2/r${i}B.grf
-    echo r-$i modified-mcsplit-experiment/random-instances2/r${i}A.grf modified-mcsplit-experiment/random-instances2/r${i}B.grf 0 >> randomplaininstances2.txt
+    python random_graph.py 10-40 0.3 > random-instances3/r${i}A.grf
+    python random_graph.py 10-40 0.3 > random-instances3/r${i}B.grf
+    echo r-$i modified-mcsplit-experiment/random-instances3/r${i}A.grf modified-mcsplit-experiment/random-instances3/r${i}B.grf 0 >> randomplaininstances3.txt
 done
