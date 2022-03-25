cf1b9627d00b3db8acf17c217a4b607b8fb3c6b7
James Trimble <james.trimble@yahoo.co.uk>
2022-03-25 22:45:31 +0000

    Add SI decision experiment

---
+++ 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/run_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+timelimit=20
+
+rm -f program-output/*
+
+mkdir -p intermediate
+rm -f intermediate/commands.txt
+
+cat ../cpaior2019-sbs-for-subgraphs-paper/experiments/instances.txt | while read inst file_a file_b _; do
+    echo ./scripts/run_one.sh $inst $file_a $file_b $timelimit >> intermediate/commands.txt
+done
+
+parallel --bar -P32 :::: intermediate/commands.txt
---
+++ 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/run_one.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -euo pipefail
+
+extratime=0
+
+instance=$1
+file_a=$2
+file_b=$3
+timelimit=$4
+
+mkdir -p instances
+mkdir -p program-output
+
+pf=../cpaior2019-sbs-for-subgraphs-paper/instances/$file_a
+tf=../cpaior2019-sbs-for-subgraphs-paper/instances/$file_b
+out=program-output/$instance
+../../vf3-instances-experiment/cpp-utils/ciaran-lad2arg-modified/lad2arg < $pf > instances/$instance.pattern.grf
+../../vf3-instances-experiment/cpp-utils/ciaran-lad2gfu/lad2gfu < $pf > instances/$instance.pattern.gfu
+../../vf3-instances-experiment/cpp-utils/ciaran-lad2arg-modified/lad2arg < $tf > instances/$instance.target.grf
+../../vf3-instances-experiment/cpp-utils/ciaran-lad2gfu/lad2gfu < $tf > instances/$instance.target.gfu
+
+../../vf3-instances-experiment/cpp-utils/is-connected/is_lad_connected < $pf > $out.connected.out
+../../vf3-instances-experiment/cpp-utils/is-connected/is_lad_connected < $tf >> $out.connected.out
+if [ "$(head -n1 $out.connected.out)" == "Connected" ]; then
+    timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/vf3lib/bin/vf3_first_match_only -u -r0 \
+        instances/$instance.pattern.grf instances/$instance.target.grf > $out.vf3.out \
+            || echo Failed $? >> $out.vf3.out
+else
+    echo "PATTERN DISCONNECTED" > $out.vf3.out
+fi
+timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/RI/ri36_with_timing ind gfu \
+        instances/$instance.target.gfu instances/$instance.pattern.gfu > $out.ri.out \
+        || echo Failed $? >> $out.ri.out
+timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/RI-DS/ri351ds_with_timing ind gfu \
+        instances/$instance.target.gfu instances/$instance.pattern.gfu > $out.ri-ds.out \
+        || echo Failed $? >> $out.ri-ds.out
+timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/glasgow-subgraph-solver/glasgow_subgraph_solver \
+    --format lad --induced $pf $tf --timeout $timelimit > $out.glasgow.out \
+        || echo Failed $? >> $out.glasgow.out
+../../vf3-instances-experiment/programs/glasgow-subgraph-solver/glasgow_subgraph_solver \
+    --no-supplementals --format lad --induced $pf $tf --timeout $timelimit > $out.glasgow-nosupp.out
+../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si --lad B $pf $tf --timeout $timelimit > $out.mcsplit-si.out
+../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si-adjmat --lad B $pf $tf --timeout $timelimit > $out.mcsplit-si-adjmat.out
+
+rm instances/$instance.pattern.grf
+rm instances/$instance.pattern.gfu
+rm instances/$instance.target.grf
+rm instances/$instance.target.gfu
