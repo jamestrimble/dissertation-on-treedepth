93ab4c98544d91fe4014dcd3d54d90bbbc35ec56
James Trimble <james.trimble@yahoo.co.uk>
2022-03-26 10:48:18 +0000

    Tweak timeout and threads

--- 14b-mcsplit-induced-si/vf3-instances-experiment/experiment/scripts/run_all.sh
+++ 14b-mcsplit-induced-si/vf3-instances-experiment/experiment/scripts/run_all.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-timelimit=5
+timelimit=1000
 
 rm -f intermediate/commands.txt
 rm -f program-output/*
@@ -11,4 +11,4 @@ cat intermediate/instances.txt | while read i count labels p nt np; do
     echo ./scripts/run_one.sh $i $labels $p $nt $np $timelimit >> intermediate/commands.txt
 done
 
-parallel --bar -P2 :::: intermediate/commands.txt
+parallel --bar -P32 :::: intermediate/commands.txt
