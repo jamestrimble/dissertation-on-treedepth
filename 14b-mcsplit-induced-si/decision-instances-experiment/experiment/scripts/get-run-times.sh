08e5b345ef940cb546fba38a9e8e84d57b20a1c9
James Trimble <james.trimble@yahoo.co.uk>
2022-03-29 12:33:02 +0100

    Fix script to get run times

--- 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/get-run-times.sh
+++ 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/get-run-times.sh
@@ -22,18 +22,18 @@ show_glasgow_time_or_timeout() {
     fi
 }
 
-echo i labels p nt np mcsplit-si mcsplit-si-dom mcsplit-si-adjmat mcsplit-si-adjmat-dom glasgow glasgow-nosupp ri ri-ds vf3
-cat intermediate/instances.txt | while read i count labels p nt np; do
+echo instance mcsplit-si mcsplit-si-dom mcsplit-si-adjmat mcsplit-si-adjmat-dom glasgow glasgow-nosupp ri ri-ds vf3
+cat ../cpaior2019-sbs-for-subgraphs-paper/experiments/instances.txt | while read instance rest; do
     echo $(
-        echo $i $labels $p $nt $np
-        cat program-output/$np-$nt-$p-$labels-$i.mcsplit-si.out | grep Time | awk '{print $3}'
-        cat program-output/$np-$nt-$p-$labels-$i.mcsplit-si-dom.out | grep Time | awk '{print $3}'
-        cat program-output/$np-$nt-$p-$labels-$i.mcsplit-si-adjmat.out | grep Time | awk '{print $3}'
-        cat program-output/$np-$nt-$p-$labels-$i.mcsplit-si-adjmat-dom.out | grep Time | awk '{print $3}'
-        show_glasgow_time_or_timeout program-output/$np-$nt-$p-$labels-$i.glasgow.out
-        cat program-output/$np-$nt-$p-$labels-$i.glasgow-nosupp.out | grep runtime | cut -d' ' -f3
-        show_time_or_timeout program-output/$np-$nt-$p-$labels-$i.ri.out
-        show_time_or_timeout program-output/$np-$nt-$p-$labels-$i.ri-ds.out
-        show_time_or_timeout program-output/$np-$nt-$p-$labels-$i.vf3.out
+        echo $instance
+        cat program-output/$instance.mcsplit-si.out | grep Time | awk '{print $3}'
+        cat program-output/$instance.mcsplit-si-dom.out | grep Time | awk '{print $3}'
+        cat program-output/$instance.mcsplit-si-adjmat.out | grep Time | awk '{print $3}'
+        cat program-output/$instance.mcsplit-si-adjmat-dom.out | grep Time | awk '{print $3}'
+        show_glasgow_time_or_timeout program-output/$instance.glasgow.out
+        cat program-output/$instance.glasgow-nosupp.out | grep runtime | cut -d' ' -f3
+        show_time_or_timeout program-output/$instance.ri.out
+        show_time_or_timeout program-output/$instance.ri-ds.out
+        show_time_or_timeout program-output/$instance.vf3.out
     )
 done
