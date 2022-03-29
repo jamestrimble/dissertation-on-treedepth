181982d7454ad9bfad22269149c14f9bfadf3de5
James Trimble <james.trimble@yahoo.co.uk>
2022-03-29 20:17:52 +0100

    Remove a temporary "if"

--- 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/run_one.sh
+++ 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/run_one.sh
@@ -23,31 +23,28 @@ out=program-output/$instance
 (../../vf3-instances-experiment/cpp-utils/is-connected/is_lad_connected < $pf || true) > $out.connected.out
 (../../vf3-instances-experiment/cpp-utils/is-connected/is_lad_connected < $tf || true) >> $out.connected.out
 
-# FIXME The following `if` is temporary!
-if grep -q 'Disconnected' $out.connected.out; then
-    if [ "$(head -n1 $out.connected.out)" == "Connected" ]; then
-        timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/vf3lib/bin/vf3_first_match_only -u -r0 \
-            instances/$instance.pattern.grf instances/$instance.target.grf > $out.vf3.out \
-                || echo Failed $? >> $out.vf3.out
-    else
-        echo "PATTERN DISCONNECTED" > $out.vf3.out
-    fi
-    timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/RI/ri36_decision_with_timing ind gfu \
-            instances/$instance.target.gfu instances/$instance.pattern.gfu > $out.ri.out \
-            || echo Failed $? >> $out.ri.out
-    timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/RI-DS/ri351ds_decision_with_timing ind gfu \
-            instances/$instance.target.gfu instances/$instance.pattern.gfu > $out.ri-ds.out \
-            || echo Failed $? >> $out.ri-ds.out
-    timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/glasgow-subgraph-solver/glasgow_subgraph_solver \
-        --format lad --induced $pf $tf --timeout $timelimit > $out.glasgow.out \
-            || echo Failed $? >> $out.glasgow.out
-    ../../vf3-instances-experiment/programs/glasgow-subgraph-solver/glasgow_subgraph_solver \
-        --no-supplementals --format lad --induced $pf $tf --timeout $timelimit > $out.glasgow-nosupp.out
-    ../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si --lad B $pf $tf --timeout $timelimit > $out.mcsplit-si.out
-    ../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si-adjmat --lad B $pf $tf --timeout $timelimit > $out.mcsplit-si-adjmat.out
-    ../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si --lad A $pf $tf --timeout $timelimit > $out.mcsplit-si-dom.out
-    ../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si-adjmat --lad A $pf $tf --timeout $timelimit > $out.mcsplit-si-adjmat-dom.out
+if [ "$(head -n1 $out.connected.out)" == "Connected" ]; then
+    timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/vf3lib/bin/vf3_first_match_only -u -r0 \
+        instances/$instance.pattern.grf instances/$instance.target.grf > $out.vf3.out \
+            || echo Failed $? >> $out.vf3.out
+else
+    echo "PATTERN DISCONNECTED" > $out.vf3.out
 fi
+timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/RI/ri36_decision_with_timing ind gfu \
+        instances/$instance.target.gfu instances/$instance.pattern.gfu > $out.ri.out \
+        || echo Failed $? >> $out.ri.out
+timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/RI-DS/ri351ds_decision_with_timing ind gfu \
+        instances/$instance.target.gfu instances/$instance.pattern.gfu > $out.ri-ds.out \
+        || echo Failed $? >> $out.ri-ds.out
+timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/glasgow-subgraph-solver/glasgow_subgraph_solver \
+    --format lad --induced $pf $tf --timeout $timelimit > $out.glasgow.out \
+        || echo Failed $? >> $out.glasgow.out
+../../vf3-instances-experiment/programs/glasgow-subgraph-solver/glasgow_subgraph_solver \
+    --no-supplementals --format lad --induced $pf $tf --timeout $timelimit > $out.glasgow-nosupp.out
+../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si --lad B $pf $tf --timeout $timelimit > $out.mcsplit-si.out
+../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si-adjmat --lad B $pf $tf --timeout $timelimit > $out.mcsplit-si-adjmat.out
+../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si --lad A $pf $tf --timeout $timelimit > $out.mcsplit-si-dom.out
+../../vf3-instances-experiment/programs/mcsplit-si/mcsplit-si-adjmat --lad A $pf $tf --timeout $timelimit > $out.mcsplit-si-adjmat-dom.out
 
 rm instances/$instance.pattern.grf
 rm instances/$instance.pattern.gfu
