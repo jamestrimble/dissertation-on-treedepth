05f85955c2b1e2907dda9be5189f80702ca0aefe
James Trimble <james.trimble@yahoo.co.uk>
2022-04-04 21:07:07 +0100

    Tweak experiment

--- 14b-mcsplit-induced-si/better-than-glasgow-instance/make_and_solve_instances.sh
+++ 14b-mcsplit-induced-si/better-than-glasgow-instance/make_and_solve_instances.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-#for i in $(seq 3 20) $(seq 100 100 1000); do
-for i in 10000 100000 1000000; do
+for i in $(seq 3 20) 100 1000 10000 100000 1000000; do
     echo $i $((i+3))
     ./make_and_solve_instance.sh $i $((i+3))
-done
+done | tee results.txt
