1f28cd669135d427a5f4cbb50a066d6442807cfb
James Trimble <james.trimble@yahoo.co.uk>
2022-04-05 13:18:51 +0100

    Fix script to get nodes

--- 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/get-nodes.sh
+++ 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/get-nodes.sh
@@ -8,9 +8,9 @@ echo instance mcsplit-si mcsplit-si-dom mcsplit-si-adjmat mcsplit-si-adjmat-dom
 cat ../cpaior2019-sbs-for-subgraphs-paper/experiments/instances.txt | while read instance rest; do
     echo $(
         echo $instance
-        cat program-output/$instance.mcsplit-si.out | grep Time | awk -f scripts/get-mcsplit-nodes.awk
-        cat program-output/$instance.mcsplit-si-dom.out | grep Time | awk -f scripts/get-mcsplit-nodes.awk
-        cat program-output/$instance.mcsplit-si-adjmat.out | grep Time | awk -f scripts/get-mcsplit-nodes.awk
-        cat program-output/$instance.mcsplit-si-adjmat-dom.out | grep Time | awk -f scripts/get-mcsplit-nodes.awk
+        cat program-output/$instance.mcsplit-si.out | awk -f scripts/get-mcsplit-nodes.awk
+        cat program-output/$instance.mcsplit-si-dom.out | awk -f scripts/get-mcsplit-nodes.awk
+        cat program-output/$instance.mcsplit-si-adjmat.out | awk -f scripts/get-mcsplit-nodes.awk
+        cat program-output/$instance.mcsplit-si-adjmat-dom.out | awk -f scripts/get-mcsplit-nodes.awk
     )
 done
