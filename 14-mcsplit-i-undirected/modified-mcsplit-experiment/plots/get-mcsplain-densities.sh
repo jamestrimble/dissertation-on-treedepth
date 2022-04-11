ccfd8aa5083b30b8125ee2346c57fc32011783a1
James Trimble <james.trimble@yahoo.co.uk>
2022-04-11 15:07:29 +0100

    Delete some commented-out code

--- 14-mcsplit-i-undirected/modified-mcsplit-experiment/plots/get-mcsplain-densities.sh
+++ 14-mcsplit-i-undirected/modified-mcsplit-experiment/plots/get-mcsplain-densities.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-## echo instance densityA densityB > densities.mcsplaininstances.txt
-## cat ../../plots-based-on-ijcai-paper/experiments/mcsplaininstances.txt | while read a b c d; do
-##     echo \
-##         $a \
-##         $(./mcsplain-density-calculator/density-calculator ~/OthersCode/cp2016-max-common-connected-subgraph-paper/datasets/$b) \
-##         $(./mcsplain-density-calculator/density-calculator ~/OthersCode/cp2016-max-common-connected-subgraph-paper/datasets/$c)
-## done | tee -a densities.mcsplaininstances.txt
-
 echo instance nA densityA nB densityB > densities.mixandmatchinstances.txt
 cat ../mcsplainmixandmatchinstances.sample.txt | while read a b c d; do
     echo \
