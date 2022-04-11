fe0d0cf9d037a08e9f3fc8f2186c999856b951a6
James Trimble <james.trimble@yahoo.co.uk>
2022-04-11 12:14:32 +0100

    Write more results to the results directory

--- 14-mcsplit-i-undirected/modified-mcsplit-experiment/summarise2.sh
+++ 14-mcsplit-i-undirected/modified-mcsplit-experiment/summarise2.sh
@@ -11,7 +11,7 @@ PROGRAMS_COMMA_SEP=$(echo $PROGRAMS | sed 's/ /,/g')
 PARTIAL_FILES=$(echo $PROGRAMS | tr ' ' '\n' | sed 's/^/partial-results\//' | sed 's/$/.txt/')
 
 cat config/vars.txt | while read var s; do   # e.g. s=runtimes, var=CPU
-    echo instance n1 density1 n2 density2 $PROGRAMS > $var.$INSTANCETYPE.txt
+    echo instance n1 density1 n2 density2 $PROGRAMS > results/$var.$INSTANCETYPE.txt
     cat $INSTANCETYPE.sample.txt | while read instance a b _; do
         echo $(
             echo $instance
@@ -20,6 +20,6 @@ cat config/vars.txt | while read var s; do   # e.g. s=runtimes, var=CPU
             for p in $PROGRAMS; do
                 cat program-output/$INSTANCETYPE/$instance.$p.out | grep $s | awk '{print $4}'
             done
-        ) >> $var.$INSTANCETYPE.txt
+        ) >> results/$var.$INSTANCETYPE.txt
     done
 done
