6cc891fb48c03a203bc598fbe02f931b6d4b0c0c
James Trimble <james.trimble@yahoo.co.uk>
2022-04-11 15:28:43 +0100

    Tweak plots

--- 14-mcsplit-i-undirected/modified-mcsplit-experiment/plots/make-plots.sh
+++ 14-mcsplit-i-undirected/modified-mcsplit-experiment/plots/make-plots.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 mkdir -p plots
 
-awk -f make-density-extremeness.awk ../results/results/runtimes.randomplaininstances2.txt > runtimes-and-extremeness.randomplaininstances2.txt
+awk -f make-density-extremeness.awk ../results/runtimes.randomplaininstances2.txt > runtimes-and-extremeness.randomplaininstances2.txt
 
 gnuplot plots.gnuplot
 sed -i -e '19s/^\(\\path.*\)/\% \1/' plots/*.tex
