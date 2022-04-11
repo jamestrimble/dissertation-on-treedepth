9b09cfb0d7e1dbdb527c0be5de7f7f88779dea70
James Trimble <james.trimble@yahoo.co.uk>
2022-04-11 10:26:23 +0100

    Move scripts to make instances to a subdirectory

---
+++ 14-mcsplit-i-undirected/modified-mcsplit-experiment/make-all-instances.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+./scripts-to-make-instances/make-ba-gnp-instances.sh
+./scripts-to-make-instances/make-barabasi-albert-instances.sh
+./scripts-to-make-instances/make-barabasi-albertx-instances.sh
+./scripts-to-make-instances/make-gnm-instances.sh
+./scripts-to-make-instances/make-mix-and-match-instances.sh
+./scripts-to-make-instances/make-random-instances2.sh
+./scripts-to-make-instances/make-random-instances3.sh
+./scripts-to-make-instances/make-random-instances4.sh
+./scripts-to-make-instances/make-random-instances.sh
