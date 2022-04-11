5d4dbde17dbc323b66bc7ce3155bad7b895bec5a
James Trimble <james.trimble@yahoo.co.uk>
2022-04-11 14:41:02 +0100

    Remove reference to a deleted script

--- 14-mcsplit-i-undirected/modified-mcsplit-experiment/make-all-instances.sh
+++ 14-mcsplit-i-undirected/modified-mcsplit-experiment/make-all-instances.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 ./scripts-to-make-instances/make-ba-gnp-instances.sh
 ./scripts-to-make-instances/make-barabasi-albert-instances.sh
-./scripts-to-make-instances/make-barabasi-albertx-instances.sh
 ./scripts-to-make-instances/make-gnm-instances.sh
 ./scripts-to-make-instances/make-mix-and-match-instances.sh
 ./scripts-to-make-instances/make-random-instances2.sh
