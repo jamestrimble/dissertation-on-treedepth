133be83ddfff9c5f63719e55b933bee788fffbf8
James Trimble <james.trimble@yahoo.co.uk>
2022-05-02 15:08:24 +0100

    Typo

--- 14-mcsplit-i-undirected/clique-algorithm-comparison-experiment/scripts/run_both_experiments.sh
+++ 14-mcsplit-i-undirected/clique-algorithm-comparison-experiment/scripts/run_both_experiments.sh
@@ -4,8 +4,8 @@ mkdir -p results
 
 ./scripts/run_all_plain.sh
 ./scripts/get-plain-run-times.sh > results/plain-runtimes.txt
-./scripts/get-plain-optimal.sh > results/plain-optima.txt
+./scripts/get-plain-optima.sh > results/plain-optima.txt
 
 ./scripts/run_all_ved.sh
 ./scripts/get-ved-run-times.sh > results/ved-runtimes.txt
-./scripts/get-ved-optimal.sh > results/ved-optima.txt
+./scripts/get-ved-optima.sh > results/ved-optima.txt
