2addc786dff95ea3d8014148c863e2b597caf6ed
James Trimble <james.trimble@yahoo.co.uk>
2022-05-02 11:42:58 +0100

    Add a space that was missing from a script

--- 14b-mcsplit-induced-si/knight-grid-experiment/experiment/scripts/run_one.sh
+++ 14b-mcsplit-induced-si/knight-grid-experiment/experiment/scripts/run_one.sh
@@ -44,7 +44,7 @@ timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/gla
 
 # -s 1000000 is a million second CPU time limit (to avoid stopping early),
 # -f is stop at first solution, -i is induced
-timeout $(($timelimit + $extratime))../../vf3-instances-experiment/programs/pathLAD/main \
+timeout $(($timelimit + $extratime)) ../../vf3-instances-experiment/programs/pathLAD/main \
     -s 1000000 -i -f \
     -p double-edged-$pf -t double-edged-$tf > $out.pathlad.out \
         || echo Failed $? >> $out.pathlad.out
