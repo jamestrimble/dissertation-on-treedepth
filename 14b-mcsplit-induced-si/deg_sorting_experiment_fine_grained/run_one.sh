638c8634b6dba064edffdadddabd58a11993b206
James Trimble <james.trimble@yahoo.co.uk>
2022-05-20 10:19:42 +0100

    Increase node limit

--- 14b-mcsplit-induced-si/deg_sorting_experiment_fine_grained/run_one.sh
+++ 14b-mcsplit-induced-si/deg_sorting_experiment_fine_grained/run_one.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 n=$1
 seed=$2
 
-./mcsp_sparse_density_experiment --node-limit 10000000 A $n 80 $seed > results/run${n}-${seed}.txt
+./mcsp_sparse_density_experiment --node-limit 100000000 A $n 80 $seed > results/run${n}-${seed}.txt
