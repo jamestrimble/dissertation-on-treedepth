76c6af4d7da982980f1b2f3f3fb3e6b7595e0a9c
James Trimble <james.trimble@yahoo.co.uk>
2022-07-05 11:42:15 +0100

    Update MCIS heur figs

--- 14-mcsplit-i-undirected/deg_sorting_experiment_fine_grained/observable-figures/convert-to-pdf.sh
+++ 14-mcsplit-i-undirected/deg_sorting_experiment_fine_grained/observable-figures/convert-to-pdf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for name in bars10 winners10 bars10directed winners10directed; do
+for filename in *.svg; do
+    name=$(basename -s .svg $filename)
     cat $name.svg | ~/Programs/inkscape/Inkscape-0a00cf5-x86_64.AppImage --pipe --export-filename=$name.pdf
 done
 
