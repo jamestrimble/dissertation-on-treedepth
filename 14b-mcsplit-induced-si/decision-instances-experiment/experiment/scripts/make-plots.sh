6e05bfe2da00d5831b5de8d16bc41463cbde1a40
James Trimble <james.trimble@yahoo.co.uk>
2022-07-14 12:32:28 +0100

    Add a missing line to plot script

--- 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/make-plots.sh
+++ 14b-mcsplit-induced-si/decision-instances-experiment/experiment/scripts/make-plots.sh
@@ -8,5 +8,6 @@ mkdir -p plots/by-family
 gnuplot scripts/plots.gnuplot
 
 sed -i -e '19s/^\(\\path.*\)/\% \1/' plots/*.tex
-#cd plots && latexmk -pdf *.tex
+cd plots && latexmk -pdf *.tex
+
 #cd plots && latexmk -pdf density-runtime-ratio*.tex
