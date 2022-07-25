70f82077587a3b415ea5ca7dd289436a89462bde
James Trimble <james.trimble@yahoo.co.uk>
2022-07-25 14:11:59 +0100

    Uncomment some lines from a plot script

--- 14-mcsplit-i-undirected/plots-based-on-ijcai-paper/more-analysis/make-plots.sh
+++ 14-mcsplit-i-undirected/plots-based-on-ijcai-paper/more-analysis/make-plots.sh
@@ -4,8 +4,6 @@ set -eu -o pipefail
 
 gnuplot scatter-plots.gnuplot
 
-###  gnuplot runtime-comparisons.gnuplot
-###  sed -i -e '19s/^\(\\path.*\)/\% \1/' plots/*.tex
-###  cd plots && latexmk -pdf *.tex
-###  #cd plots && latexmk -pdf *runtime-ratio*.tex
-###  #cd plots && latexmk -pdf sip-runtime-ratio-vs-solution-size.tex
+gnuplot runtime-comparisons.gnuplot
+sed -i -e '19s/^\(\\path.*\)/\% \1/' plots/*.tex
+cd plots && latexmk -pdf *.tex
